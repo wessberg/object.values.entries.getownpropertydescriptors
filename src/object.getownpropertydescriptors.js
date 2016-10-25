6bca42ed249d416104e729f95e7752dbfb26836f
wessberg <frederikwessberg@gmail.com>
2016-10-25 20:00:32 +0200

    Added feature detection to Object.getOwnPropertyDescriptors polyfill

--- src/object.getownpropertydescriptors.js
+++ src/object.getownpropertydescriptors.js
@@ -15,58 +15,60 @@ if (typeof Reflect === 'undefined') {
   };
 }
 
-Object.defineProperty(
-  Object,
-  'getOwnPropertyDescriptors',
-  {
-    configurable: true,
-    writable: true,
-    value: function getOwnPropertyDescriptors(genericObject) {
-      // Let `obj` be ? `ToObject(O)`
-      if (Object(genericObject) !== genericObject) {
-        throw new Error('Argument should be an object');
-      }
+if (!("getOwnPropertyDescriptors" in Object)) {
+	Object.defineProperty(
+	  Object,
+	  'getOwnPropertyDescriptors',
+	  {
+	    configurable: true,
+	    writable: true,
+	    value: function getOwnPropertyDescriptors(genericObject) {
+	      // Let `obj` be ? `ToObject(O)`
+	      if (Object(genericObject) !== genericObject) {
+	        throw new Error('Argument should be an object');
+	      }
 
-      // Let `ownKeys` be the result of calling ? `obj.[[OwnPropertyKeys]]()`
-      let ownKeys;
-      try {
-        ownKeys = Reflect.ownKeys(genericObject);
-      } catch(e) {
-        throw new Error('Unable to retrieve own keys');
-      }
+	      // Let `ownKeys` be the result of calling ? `obj.[[OwnPropertyKeys]]()`
+	      let ownKeys;
+	      try {
+	        ownKeys = Reflect.ownKeys(genericObject);
+	      } catch(e) {
+	        throw new Error('Unable to retrieve own keys');
+	      }
 
-      // Let `descriptors` be ? `ObjectCreate(%ObjectPrototype%)`
-      let descriptors;
-      try {
-        descriptors = Object.create(Object.prototype);
-      } catch(e) {
-        throw new Error('Unable to create an instance of Object.prototype');
-      }
+	      // Let `descriptors` be ? `ObjectCreate(%ObjectPrototype%)`
+	      let descriptors;
+	      try {
+	        descriptors = Object.create(Object.prototype);
+	      } catch(e) {
+	        throw new Error('Unable to create an instance of Object.prototype');
+	      }
 
-      // Repeat, for each element `key` of `ownKeys` in List order
-      for (let key of ownKeys) {
+	      // Repeat, for each element `key` of `ownKeys` in List order
+	      for (let key of ownKeys) {
 
-        // Let `desc` be the result of ? `obj.[[GetOwnProperty]](key)`
-        // Let `descriptor` be ? `FromPropertyDescriptor(desc)`
-        let descriptor = Reflect.getOwnPropertyDescriptor(genericObject, key);
+	        // Let `desc` be the result of ? `obj.[[GetOwnProperty]](key)`
+	        // Let `descriptor` be ? `FromPropertyDescriptor(desc)`
+	        let descriptor = Reflect.getOwnPropertyDescriptor(genericObject, key);
 
-        if (typeof descriptor !== 'undefined') {
-          // Let `status` be the result of ? `CreateDataProperty(descriptors, key, descriptor)`
-          try {
-            Reflect.defineProperty(descriptors, key, {
-              configurable: true,
-              enumerable: true,
-              writable: true,
-              value: descriptor
-            });
-          } catch(e) {
-            throw new Error('Unable to create a data propoerty');
-          }
-        }
-      }
+	        if (typeof descriptor !== 'undefined') {
+	          // Let `status` be the result of ? `CreateDataProperty(descriptors, key, descriptor)`
+	          try {
+	            Reflect.defineProperty(descriptors, key, {
+	              configurable: true,
+	              enumerable: true,
+	              writable: true,
+	              value: descriptor
+	            });
+	          } catch(e) {
+	            throw new Error('Unable to create a data propoerty');
+	          }
+	        }
+	      }
 
-      // Return `descriptors`
-      return descriptors;
-    }
-  }
-);
+	      // Return `descriptors`
+	      return descriptors;
+	    }
+	  }
+	);
+}
